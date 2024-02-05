321009ed4ea473ef43c2649ee4805861b000ec7a
Alexandre Forestier <alexandreforestier1107@gmail.com>
2024-02-05 04:24:57 +0000

    Mise à jour des dépendances Symfony dans composer.json et composer.lock

---
+++ src/Entity/Smartphone.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\SmartphoneRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: SmartphoneRepository::class)]
+class Smartphone
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+}
